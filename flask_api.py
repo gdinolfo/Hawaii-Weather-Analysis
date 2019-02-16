5ee659549d990bdc938996f4e3fcf6684f1301d8
Glen D <glen33dino@gmail.com>
2019-02-16 10:23:24 -0700

    update flask_api

--- flask_api.py
+++ flask_api.py
@@ -1,10 +1,8 @@
-# Import all necessary modules and create Flask app
-
 from flask import Flask, jsonify, render_template
 import pandas as pd
 import datetime as dt
 
-# create app
+# Flask Setup
 app = Flask(__name__)
 
 # set up sqlalchemy engine
@@ -44,13 +42,13 @@ def prev_yr_precip():
              WHERE date > "{prev_yr_start}"')
     return jsonify(pd.read_sql(query, engine).to_dict(orient='records'));
 
-# json list of stations from the dataset.
+#list of stations from the dataset.
 @app.route('/api/v1.0/stations')
 def station_list():
     query = 'SELECT station, name FROM station'
     return jsonify(pd.read_sql(query, engine).to_dict(orient='records'));
 
-# json list of Temperature Observations (tobs) for the previous year
+#list of Temperature Observations (tobs) for the previous year
 @app.route('/api/v1.0/tobs')
 def prev_yr_temps():
     prev_yr_start = (dt.date(2017,8,23) - dt.timedelta(days=365)).isoformat()
@@ -58,18 +56,18 @@ def prev_yr_temps():
              WHERE date > "{prev_yr_start}"')
     return jsonify(pd.read_sql(query, engine).to_dict(orient='records'));
 
-# json list of the min temp, avg temp, max temp in date range
+#list of the min temp, avg temp, max temp in date range
 
 @app.route('/api/v1.0/<start>')
 
-# one variant where only start date given
+#only start date given
 def temps_startOnly(start):
     query = (f'SELECT AVG(tobs) AS "Average Temperature", MIN(tobs) \
              AS "Minimum Temperature", MAX(tobs) AS "Maximum Temperature" \
              FROM measurement WHERE date >= "{start}"')
     return jsonify(pd.read_sql(query, engine).to_dict(orient='records'));
 
-# one variant where both start and end date given
+#both start and end date given
 @app.route('/api/v1.0/<start>/<end>')
 def temps_startAndEnd(start, end):
     query = (f'SELECT AVG(tobs) AS "Avg Temp", MIN(tobs) \
